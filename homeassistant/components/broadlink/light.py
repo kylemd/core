aae5031bce2335846d9cce145c8017e6fcefea5e
kylemd <kyle.m.drayton@gmail.com>
2021-12-15 15:23:52 +1100

    Add support for Broadlink LB2 bulbs
    python-broadlink added support for the LB2 light bulb but it wasn't added to the Home Assistant integration. This adds the support necessary.

--- homeassistant/components/broadlink/light.py
+++ homeassistant/components/broadlink/light.py
@@ -32,6 +32,8 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
     if device.api.type == "LB1":
         lights.append(BroadlinkLight(device))
+    if device.api.type == "LB2":
+        lights.append(BroadlinkLight(device))
 
     async_add_entities(lights)
 
